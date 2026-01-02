15095d86ad6d6c554b4017eb5c9586d10c43211c
leozin404 <leosilva200311@gmail.com>
2026-01-02 11:31:24 -0300

    changes

--- cpp.cpp
+++ cpp.cpp
@@ -1,10 +1,10 @@
-#include iostream
+#include <iostream>
 using namespace std;
 
 int main(void){
     cout << "Its my first program!" << endl;
     for (int i = 0; i < 3; i++){
-        cout << "Hi, it's Leo!"; << endl;
+        cout << "Hi, it's Leo!" << endl;
     }
     return 0;
 }
